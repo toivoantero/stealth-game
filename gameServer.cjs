401956eece7c9fb42995094ec2c092d7dd11b161
toivoantero <125696622+toivoantero@users.noreply.github.com>
2025-05-13 16:59:27 +0300

    yritän saada toimimaan render.com:ssa.

--- gameServer.cjs
+++ gameServer.cjs
@@ -15,5 +15,8 @@ server.listen(port, () => {
 });
 
 // Staattisten tiedostojen palveleminen (esim. index.html, index.js, jne.)
-app.use(express.static(path.join(__dirname, 'index.html')));
+//app.use(express.static(path.join(__dirname, 'index.html')));
   
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
